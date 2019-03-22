f380346dd540cb0d0a189090fdde0a816e439d58
Matthew Kijowski <matthewkijowski@gmail.com>
2019-03-22 14:01:51 -0400

    fixes, actually working yay...

--- GeoAssignCoords.py
+++ GeoAssignCoords.py
@@ -6,30 +6,43 @@ import pandas as pd
 def main():
     colors = vtk.vtkNamedColors()
 
+    ## Latitude and Lognitude setup
     g = vtk.vtkMutableDirectedGraph()
     latitude = vtk.vtkDoubleArray()
     latitude.SetName("latitude")
     longitude = vtk.vtkDoubleArray()
     longitude.SetName("longitude")
 
+    ## Use Pandas to import lat/long from data
     latlong = pd.read_csv('data/unique-sorted-lat-long.dat', delimiter = ' ')
-
     for i in range(0, len(latlong)):
             g.AddVertex()
             latitude.InsertNextValue(latlong.Latitude[i])
             longitude.InsertNextValue(latlong.Longitude[i])
 
+    ## Create assign from latlong
     g.GetVertexData().AddArray(latitude)
     g.GetVertexData().AddArray(longitude)
-
     assign = vtk.vtkGeoAssignCoordinates()
     assign.SetInputData(g)
-
     assign.SetLatitudeArrayName("latitude")
     assign.SetLongitudeArrayName("longitude")
-    assign.SetGlobeRadius(1.0)
+    assign.SetGlobeRadius(60.0)
     assign.Update()
+    assignMapper = vtk.vtkGraphMapper()
+    assignMapper.SetInputConnection(assign.GetOutputPort())
+    assignActor = vtk.vtkActor()
+    assignActor.SetMapper(assignMapper)
 
+
+    ## Graph to poly
+    graphtopoly = vtk.vtkGraphToPolyData()
+    graphtopoly.SetInputData(assign.GetOutput())
+    graphtopoly.Update()
+
+
+
+    ## Import Tom's globe.obj
     globeobjPath = "/home/mkijowski/git/vtk-assignment-1/data/globe.obj"
 
     globe = vtk.vtkOBJReader()
@@ -41,23 +54,46 @@ def main():
     globeActor = vtk.vtkActor()
     globeActor.SetMapper(globeMapper)
 
+    ## Import Earth.source
+    earthSource = vtk.vtkEarthSource()
+    earthSource.OutlineOn()
+    earthSource.Update()
+    earthMapper = vtk.vtkPolyDataMapper()
+    earthMapper.SetInputConnection(earthSource.GetOutputPort())
+    earthActor = vtk.vtkActor()
+    earthActor.SetMapper(earthMapper)
+
+    ## Sphere markers for location data
+    sphere = vtk.vtkSphereSource()
+    sphere.SetThetaResolution(7)
+    sphere.SetPhiResolution(7)
+
+    ## Create glyph
     glyph = vtk.vtkGlyph3D()
-    glyph.SetInputConnection(globe.GetOutputPort())
-    glyph.SetSourceConnection(assign.GetOutputPort())
+    #glyph.SetInputConnection(earthSource.GetOutputPort())
+    glyph.SetInputConnection(graphtopoly.GetOutputPort())
+    #glyph.SetSourceConnection(graphtopoly.GetOutputPort())
+    glyph.SetSourceConnection(sphere.GetOutputPort())
     glyph.SetVectorModeToUseNormal()
     glyph.SetScaleModeToScaleByVector()
-    glyph.SetScaleFactor(0.25)
+    # Size for globe
+    glyph.SetScaleFactor(2)
 
+    #size for earth
+    #glyph.SetScaleFactor(.01)
 
-
-    pingMapper = vtk.vtkGraphMapper()
+    ## 
+    pingMapper = vtk.vtkPolyDataMapper()
     pingMapper.SetInputConnection(glyph.GetOutputPort())
     pingActor = vtk.vtkActor()
     pingActor.SetMapper(pingMapper)
+    pingActor.GetProperty().SetColor(.5,0,0)
 
     ren = vtk.vtkRenderer()
     ren.AddActor(pingActor)
+#    ren.AddActor(assignActor)
     ren.AddActor(globeActor)
+#    ren.AddActor(earthActor)
 
     iren = vtk.vtkRenderWindowInteractor()
     renWin = vtk.vtkRenderWindow()
